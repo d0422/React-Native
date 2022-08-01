511ff25f28cd1882b858f2c7b753ebf86fb68d60
d0422 <rlfehd2013@naver.com>
2022-08-01 21:54:15 +0900

    :zap: View

--- Weather/App.js
+++ Weather/App.js
@@ -1,23 +1,48 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import { StatusBar } from "expo-status-bar";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>React-Native</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#006CB8",
   },
-  text: {
-    fontSize: 28,
+  cityName: {
+    color: "black",
+    fontSize: 68,
+    fontWeight: "600",
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    borderTopWidth: 3,
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    fontSize: 60,
   },
 });
