70aafcfc22140b2d4ec8343fd6ebdd1ff93e17bc
d0422 <rlfehd2013@naver.com>
2022-08-07 19:40:18 +0900

    :zap: todo add

--- WorkHardTravelHardApp/App.js
+++ WorkHardTravelHardApp/App.js
@@ -6,6 +6,7 @@ import {
   TouchableHighlight,
   TouchableOpacity,
   TextInput,
+  ScrollView,
 } from "react-native";
 import { useState } from "react";
 import { theme } from "./colors";
@@ -16,6 +17,20 @@ export default function App() {
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
+  const [toDos, setToDos] = useState({});
+  const addTodo = () => {
+    if (text === "") {
+      return;
+    }
+    // todo 저장
+    const newToDos = {
+      ...toDos,
+      [Date.now()]: { text, work: working },
+    };
+    setToDos(newToDos);
+    setText("");
+  };
+  console.log(toDos);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -39,11 +54,20 @@ export default function App() {
         </TouchableOpacity>
       </View>
       <TextInput
+        onSubmitEditing={addTodo}
         onChangeText={onChangeText}
+        returnKeyType="done"
         style={styles.input}
         value={text}
         placeholder={working ? "Add a Todo" : "Where do you want to go?"}
       ></TextInput>
+      <ScrollView>
+        {Object.keys(toDos).map((key) => (
+          <View key={key} style={styles.toDo}>
+            <Text style={styles.todotext}>{toDos[key].text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -56,7 +80,7 @@ const styles = StyleSheet.create({
   },
   header: {
     flexDirection: "row",
-    marginTop: "100",
+    marginTop: 10,
     justifyContent: "space-between",
   },
   btnText: {
@@ -70,5 +94,18 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     marginTop: 20,
     fontSize: 18,
+    marginVertical: 20,
+  },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    borderRadius: 15,
+  },
+  todotext: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
   },
 });
